fb0d1524cf3e2ffaad2103997c2e827e0280c166
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-06-21 14:33:29 +0700

    chore: delete toy experiment source

--- src/experiment.py
+++
@@ -1,7 +0,0 @@
-import mlflow
-
-mlflow.set_experiment("test_experiment_1")
-
-with mlflow.start_run():
-    mlflow.log_param('model', 'llama-3.3-70b-versatile')
-    mlflow.log_param('latency', 1.06)
