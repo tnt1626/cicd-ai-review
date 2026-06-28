54c6057a730dc9d418bdb16e98c897c46264ad86
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-06-28 23:44:23 +0700

    feat(iac): add terraform remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,19 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://sgp1.digitaloceanspaces.com"
+    }
+    bucket                      = "cicd-ai-review-tfstate"
+    key                         = "terraform.tfstate"
+    region                      = "sgp1"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
 }
 
 provider "digitalocean" {
